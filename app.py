d76dd6b23492156479bb3147fda41a0adbed71da
FuShumin <332587268@qq.com>
2023-11-09 16:09:39 +0800

    v2 - 生产环境

--- app.py
+++ app.py
@@ -6,14 +6,26 @@ from linear_programming import optimize_stock_distribution_percentage, \
     calculate_stock_percentage_change, map_index_to_readable_result
 from datetime import datetime
 from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
+# app.config['JSON_AS_ASCII'] = False
+# Flask==2.3.0及以上
+app.json.ensure_ascii = False
 
 
+# def get_database_connection():
+#     """Establish and return a database connection."""
+#     return pymysql.connect(host='10.1.20.196', port=3306, user='root', password='nti56.com', database='lcs')
 def get_database_connection():
     """Establish and return a database connection."""
-    return pymysql.connect(host='10.1.20.196', port=3306, user='root', password='nti56.com', database='lcs')
+    host = os.environ.get('DB_HOST', '10.1.20.196')
+    port = int(os.environ.get('DB_PORT', 3306))
+    user = os.environ.get('DB_USER', 'root')
+    password = os.environ.get('DB_PASSWORD', 'nti56.com')
+    database = os.environ.get('DB_NAME', 'lcs')
+
+    return pymysql.connect(host=host, port=port, user=user, password=password, database=database)
 
 
 def execute_query(cursor, query, params=None):
@@ -24,16 +36,32 @@ def execute_query(cursor, query, params=None):
     return pd.DataFrame(rows, columns=fields)
 
 
+# 用于处理排序参数的函数
+def sort_by_fields(data, order_by_items):
+    for order_item in reversed(order_by_items):
+        field = order_item.get('field')
+        order = order_item.get('order', 'asc')
+
+        # Make sure that the field exists in all dictionaries
+        if all(field in d for d in data):
+            data.sort(key=lambda x: x[field], reverse=(order == 'desc'))
+        else:
+            print(f"Field '{field}' not found in all dictionaries.")
+    return data
+
+
 def fetch_and_calculate(client_id):
     client_id = client_id
+    # client_id = 1691005891402997762
     # 连接数据库
-
+    # %%
     # 执行SQL查询
     with get_database_connection() as conn:
         with conn.cursor() as cursor:
-            query = ("SELECT material_code AS item_code, start_code, end_code FROM product_algorithmic_rule WHERE "
-                     "deleted = 0 "
-                     "AND client_id = %s;")
+            query = (
+                "SELECT material_code AS item_code, start_code, end_code FROM lcs_dispatch_cp_algorithmic_rule WHERE "
+                "deleted = 0 "
+                "AND client_id = %s;")
             df_special_rules = execute_query(cursor, query, (client_id,))  # 特殊规则
 
             query = "SELECT warehouse_code, item_code, available_stock FROM stock WHERE client_id = %s;"
@@ -47,8 +75,27 @@ def fetch_and_calculate(client_id):
                      "WHERE onload_stock > 0 AND client_id = %s;")
             df_onload_stock = execute_query(cursor, query, (client_id,))  # 在途库存
 
-            query = "SELECT warehouse_code, item_code, min_stock FROM stock_safe_config WHERE client_id = %s;"
+            query = """
+                SELECT t.warehouse_code, t.item_code, t.min_stock
+                FROM (
+                    SELECT c.*, i.name AS warehouse_name
+                    FROM stock_safe_config c
+                    LEFT JOIN stock_warehouse_info i ON i.code = c.warehouse_code
+                    WHERE c.deleted = 0
+                ) t
+                WHERE t.deleted = 0 AND client_id = %s;"""
             df_min_safe = execute_query(cursor, query, (client_id,))  # 牌号最小安全库存
+
+            query = """
+                SELECT t.warehouse_code, t.item_code, t.max_stock
+                FROM (
+                    SELECT c.*, i.name AS warehouse_name
+                    FROM stock_safe_config c
+                    LEFT JOIN stock_warehouse_info i ON i.code = c.warehouse_code
+                    WHERE c.deleted = 0
+                ) t
+                WHERE t.deleted = 0 AND client_id = %s;"""
+            df_max_safe = execute_query(cursor, query, (client_id,))  # 牌号最大安全库存
 
             query = """
                 SELECT
@@ -62,11 +109,12 @@ def fetch_and_calculate(client_id):
             """
             df_item_attributes = execute_query(cursor, query, (client_id,))  # 成品属性
 
-            query = "SELECT item_code, item_name FROM stock_safe_config WHERE client_id = %s;"
+            query = ("SELECT code AS item_code, name AS item_name FROM material_info WHERE client_id = %s AND "
+                     "is_delete = 0;")
             df_item_info = execute_query(cursor, query, (client_id,))  # 物品信息
 
             # 从 warehouse_info 表中找到 code（即 warehouse_code）对应的 name
-            query = "SELECT code, name FROM stock_warehouse_info WHERE client_id = %s;"
+            query = "SELECT code, name FROM stock_warehouse_info WHERE client_id = %s AND is_delete = 0;"
             df_warehouse_info = execute_query(cursor, query, (client_id,))  # 仓库信息
 
     # SECTION 当前库存处理
@@ -91,7 +139,16 @@ def fetch_and_calculate(client_id):
             i = warehouse_to_index[warehouse_code]
             j = item_to_index[item_code]
             current_stock[i, j] = row['available_stock']
-
+    # 步骤 2: 创建一个 NumPy 数组
+    current_stock_report = np.zeros((n_warehouses, m_goods), dtype=float)
+    # 步骤 3: 填充数组
+    for _, row in df_current_stock.iterrows():
+        warehouse_code = row['warehouse_code']
+        item_code = row['item_code']
+        if warehouse_code in warehouse_to_index and item_code in item_to_index:
+            i = warehouse_to_index[warehouse_code]
+            j = item_to_index[item_code]
+            current_stock_report[i, j] = row['available_stock']
     # SECTION 最大库容处理
     # 将 'safe_stock' 列转换为数字
     df_max_stock['safe_stock'] = pd.to_numeric(df_max_stock['safe_stock'], errors='coerce')
@@ -100,11 +157,11 @@ def fetch_and_calculate(client_id):
     # 将在途库存转换为字典形式，以便更容易地访问
     onload_stock_dict = df_onload_stock.set_index('warehouse_code').to_dict()['onload_stock']
 
-    # 遍历 df_max_stock DataFrame，如果在 onload_stock_dict 中找到相应的仓库，就更新其最大库容
-    for idx, row in df_max_stock.iterrows():
-        warehouse_code = row['warehouse_code']
-        if warehouse_code in onload_stock_dict:
-            df_max_stock.loc[idx, 'safe_stock'] -= onload_stock_dict[warehouse_code]
+    # # 遍历 df_max_stock DataFrame，如果在 onload_stock_dict 中找到相应的仓库，就更新其最大库容
+    # for idx, row in df_max_stock.iterrows():
+    #     warehouse_code = row['warehouse_code']
+    #     if warehouse_code in onload_stock_dict:
+    #         df_max_stock.loc[idx, 'safe_stock'] -= onload_stock_dict[warehouse_code]
 
     # 确保所有的 'safe_stock' 值都是非负的
     df_max_stock['safe_stock'] = df_max_stock['safe_stock'].apply(lambda x: max(0, x))
@@ -121,6 +178,7 @@ def fetch_and_calculate(client_id):
     # SECTION 最小安全库存处理
     # 确保 'min_stock' 列是数字格式
     df_min_safe['min_stock'] = pd.to_numeric(df_min_safe['min_stock'], errors='coerce')
+
     # 创建一个 NumPy 数组存放最小安全库存，维度与 current_stock 相同
     min_safety_stock = np.zeros((n_warehouses, m_goods), dtype=int)
 
@@ -142,11 +200,16 @@ def fetch_and_calculate(client_id):
             continue
 
         min_safety_stock[i, k] = row['min_stock']
-    # 创建一个 NumPy 数组存放最小安全库存，维度与 current_stock 相同
-    min_safety_stock = np.zeros((n_warehouses, m_goods), dtype=int)
+
+    # SECTION： 牌号最大安全库存处理
+    # 确保 'max_stock' 列是数字格式
+    df_max_safe['max_stock'] = pd.to_numeric(df_max_safe['max_stock'], errors='coerce')
+
+    # 创建一个 NumPy 数组存放最大安全库存，维度与 current_stock 相同
+    max_safety_stock = np.full((n_warehouses, m_goods), max_stock_per_warehouse[:, np.newaxis], dtype=int)
 
     # 填充数组
-    for _, row in df_min_safe.iterrows():
+    for _, row in df_max_safe.iterrows():
         warehouse_code = row['warehouse_code']
         item_code = row['item_code']
 
@@ -162,7 +225,7 @@ def fetch_and_calculate(client_id):
         if i == -1 or k == -1:
             continue
 
-        min_safety_stock[i, k] = row['min_stock']
+        max_safety_stock[i, k] = row['max_stock']
 
     # SECTION: 特殊规则
     # 初始化一个空列表用于存储转换后的规则
@@ -189,12 +252,12 @@ def fetch_and_calculate(client_id):
 
     # 将列表转换为DataFrame
     df_special_rules_index = pd.DataFrame(special_rules_index_list)
-
+    # %%
     # SECTION 调用线性规划算法 获取actions
     try:
         actions = optimize_stock_distribution_percentage(current_stock, max_stock_per_warehouse,
-                                                                 min_safety_stock,
-                                                                 df_special_rules_index)
+                                                         min_safety_stock, max_safety_stock,
+                                                         df_special_rules_index)
     except Exception as e:
         print(f"Error: {e}")
         actions = None  # 或者你可以设置一个其它的默认值
@@ -254,12 +317,23 @@ def fetch_and_calculate(client_id):
     # 创建一个从 item_name 到 item_code 的映射
     name_to_item_code = {name: code for code, name in item_code_to_name.items()}
 
-    # 遍历 readable_actions 列表
-    for start_warehouse_name, target_warehouse_name, item_name, transfer_qty in readable_actions:
-        # 使用 name_to_warehouse_code 和 name_to_item_code 映射找到对应的 warehouse_code 和 item_code
-        start_warehouse_code = name_to_warehouse_code.get(start_warehouse_name, None)
-        target_warehouse_code = name_to_warehouse_code.get(target_warehouse_name, None)
-        item_code = name_to_item_code.get(item_name, None)
+    # 遍历actions表
+    for start_warehouse_idx, target_warehouse_idx, item_code_idx, transfer_qty in actions:
+        # Fetch item_code and warehouse names directly using item_code_idx and warehouse indexes
+        item_code = [k for k, v in item_to_index.items() if v == item_code_idx][0]
+        item_name = item_code_to_name.get(item_code, "Unknown Item")
+        start_warehouse_code = [k for k, v in warehouse_to_index.items() if v == start_warehouse_idx][0]
+        target_warehouse_code = [k for k, v in warehouse_to_index.items() if v == target_warehouse_idx][0]
+        start_warehouse_name = warehouse_code_to_name.get(start_warehouse_code, "Unknown Warehouse")
+        target_warehouse_name = warehouse_code_to_name.get(target_warehouse_code, "Unknown Warehouse")
+        #
+        # # 遍历 readable_actions 列表
+        # for start_warehouse_name, target_warehouse_name, item_name, transfer_qty in readable_actions:
+        #     # 使用 name_to_warehouse_code 和 name_to_item_code 映射找到对应的 warehouse_code 和 item_code
+        #     start_warehouse_code = name_to_warehouse_code.get(start_warehouse_name, None)
+        #     target_warehouse_code = name_to_warehouse_code.get(target_warehouse_name, None)
+        #
+        #     item_code = name_to_item_code.get(item_name, None)
 
         # 如果找不到对应的仓库或商品代码，跳过这一行
         if start_warehouse_code is None or item_code is None:
@@ -270,18 +344,18 @@ def fetch_and_calculate(client_id):
         item_code_idx = item_to_index.get(item_code, -1)
 
         # 从 current_stock 中获取起始仓库的当前库存
-        current_stock_qty = current_stock[start_warehouse_idx, item_code_idx]
+        current_stock_qty = current_stock_report[start_warehouse_idx, item_code_idx]
 
         # 从 item_code_attribute_map 中获取成品属性，type
         item_attribute_info = item_code_attribute_map.get(item_code,
                                                           {'attr_value': '未知成品属性', 'attr_id': '未知ID'})
         item_attribute = item_attribute_info['attr_value']
         if item_attribute == '1':
-            item_attribute = '在制烟'
+            item_attribute = '在制品'
         elif item_attribute == '0':
             item_attribute = '成品烟'
 
-        item_attribute_id = item_attribute_info['attr_id']
+        item_attribute_id = item_attribute_info['attr_value']
 
         # 将所有信息整理成一个列表
         # row = [start_warehouse_name, start_warehouse_code, target_warehouse_name, target_warehouse_code, item_name,
@@ -301,11 +375,13 @@ def fetch_and_calculate(client_id):
             "cigarette_type_name": item_attribute,
             "created_time": current_time,
             "recommend_qty": int(transfer_qty) if isinstance(transfer_qty, np.int64) else transfer_qty,
-            "store_qty": int(current_stock_qty) if isinstance(current_stock_qty, np.int64) else current_stock_qty,
+            # "store_qty": int(current_stock_qty) if isinstance(current_stock_qty, np.int64) else current_stock_qty,
+            "store_qty": float(current_stock_qty) if isinstance(current_stock_qty,
+                                                                (np.int64, np.float64)) else current_stock_qty,
         }
 
         final_report.append(row_dict)
-
+    # %%
     return final_report
 
 
@@ -316,17 +392,30 @@ def get_final_report():
     regulation_name = params.get('regulation_name', None)
     cigarette_type = params.get('cigarette_type', None)
     send_code = params.get('send_code', None)
+    order_by_items = request.json.get('orderByItem', [])  # 从 JSON 获取 'orderByItem'
     try:
         final_report = fetch_and_calculate(client_id)
+        # Fuzzy search for regulation_name
         if regulation_name:
-            final_report = [x for x in final_report if x.get('regulation_name') == regulation_name]
+            final_report = [x for x in final_report if regulation_name.lower() in x.get('regulation_name', '').lower()]
+
         if cigarette_type:
             final_report = [x for x in final_report if x.get('cigarette_type') == cigarette_type]
         if send_code:
             final_report = [x for x in final_report if x.get('send_code') == send_code]
-        return jsonify({"status": "success", "data": final_report})
+
+        # 使用排序函数
+        final_report = sort_by_fields(final_report, order_by_items)
+
+        return jsonify({"code": 0, "data": final_report, "message": "操作成功。"})
+
     except Exception as e:
-        return jsonify({"status": "error", "message": str(e)})
+        return jsonify({"code": -1, "message": str("请检查最小安全库存小于仓库库容以及两者不为负数。")}), 400
+
+
+@app.route('/test', methods=['GET'])
+def test():
+    return jsonify({"code": 0, "data": "操作成功。", "message": "操作成功。"})
 
 
 if __name__ == '__main__':
--- web_pred.py
+++ web_pred.py
@@ -8,19 +8,14 @@ import os
 
 # 加载 YOLO 模型
 # model = YOLO('./runs/detect/train6/weights/best.pt')
-model = YOLO('/Users/apple/Downloads/train8/weights/best.pt')
-label_info = {
-    0: '双喜(硬经典)',
-    1: '双喜(硬经典1906)',
-    2: '双喜(花悦)',
-    3: '双喜（莲香）',
-    4: '双喜(软经典)'
-}
+# model = YOLO('/Users/apple/Downloads/train8/weights/best.pt')
+# model.export(format='onnx', opset=11)
+model = YOLO('/Users/apple/Downloads/train8/weights/best.onnx', task='detect')
 
 
 def predict(inp):
     # 使用 YOLO 进行预测，并保存结果
-    results = model(inp, save=True, conf=0.5)
+    results = model(inp, save=True, conf=0.5, imgsz=[640, 640])
 
     # 获取保存目录
     save_dir = results[0].save_dir
@@ -37,8 +32,7 @@ iface = gr.Interface(
     fn=predict,  # 预测函数
     inputs=gr.inputs.Image(type="pil", label="上传你要检测的图片"),  # 输入类型和标签
     outputs=gr.outputs.Image(type="pil", label="预测结果"),  # 输出类型和标签
-    description="标签信息：<br>0-双喜(硬经典)<br>1-双喜(硬经典1906)<br>2-双喜(花悦)<br>3-双喜（莲香）<br>4-双喜(软经典)"
+    # description="标签信息：<br>0-双喜(硬经典)<br>1-双喜(硬经典1906)<br>2-双喜(花悦)<br>3-双喜（莲香）<br>4-双喜(软经典)"
 )
-
 
 iface.launch()
